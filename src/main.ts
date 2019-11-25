0c9ee4c18785ecb4e853aae21c5cf006a6263a32
Eli Mensch <eli.mensch@gmail.com>
2019-11-25 01:08:39 -0500

    Build compiler module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
-import { ASTElement } from './types';
+/*
+  Subset of (simplified) AST types converted from: 
+  https://github.com/vuejs/vue/blob/dev/flow/compiler.js
+*/
+interface ASTAttr {
+  name: string;
+  value: any;
+  [key: string]: any;
+}
 
-type MatcherType = string | string[] | RegExp;
+interface ASTElement {
+  attrsList: Array<ASTAttr>;
+  attrsMap: { [key: string]: any };
+  [key: string]: any;
+}
 
-const createAttributeRemover = (matcher: MatcherType) => {
+export type MatcherType = string | string[] | RegExp;
+
+export interface AttributeRemoverModule {
+  preTransformNode: (AST: ASTElement) => ASTElement;
+}
+
+const createAttributeRemover = (
+  matcher: MatcherType
+): AttributeRemoverModule => {
+  const testFn = getTestFunction(matcher);
+
   const preTransformNode = (AST: ASTElement) => {
     const { attrsList, attrsMap } = AST;
 
-    const attrsToDelete = attrsList.filter(({ name }) => regex.test(name));
+    const attrsToDelete = attrsList.filter(({ name }) => testFn(name));
 
     attrsToDelete.forEach(({ name: attrName }) => {
       if (attrsMap[attrName] !== undefined) {
@@ -18,6 +40,32 @@ const createAttributeRemover = (matcher: MatcherType) => {
       attrsList.splice(idx, 1);
     });
 
-    return astElement;
+    return AST;
+  };
+
+  return {
+    preTransformNode
+  };
+};
+
+type TestFunction = (name: string) => boolean;
+
+const getTestFunction = (matcher: MatcherType): TestFunction => {
+  if (typeof matcher === "string") {
+    return name => name === matcher;
+  } else if (matcher instanceof RegExp) {
+    return matcher.test.bind(matcher);
+  } else if (
+    Array.isArray(matcher) &&
+    matcher.every(matchValue => typeof matchValue === "string")
+  ) {
+    const set = new Set(matcher);
+    return set.has.bind(set);
   }
-}
+
+  throw new Error(
+    "Matcher must be a string, array of strings, or a regular expression"
+  );
+};
+
+export default createAttributeRemover;
